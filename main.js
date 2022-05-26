f6600232af253d46563bd651fa476b7638f0a24a
Hector Haro H <hector8.haro@gmail.com>
2022-05-26 18:34:43 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const URL = "https://api.thedogapi.com/v1/images/search";
+const img = document.querySelector("img");
+const title = document.getElementById("title");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const lifeSpan = document.getElementById("lifeSpan");
+const button = document.getElementById("button");
+
+async function getDogs2() {
+    const res = await fetch(URL);
+    const data = await res.json();
+    img.src = data[0].url;
+    if (data[0].breeds[0]) {
+      title.innerText = data[0].breeds[0].name;
+      weight.innerText = "Peso: " + data[0].breeds[0].weight.metric + " kg";
+      height.innerText = "Altura: " + data[0].breeds[0].height.metric + " cm";
+      lifeSpan.innerText = "Esperanza de vida: " + data[0].breeds[0].life_span;
+    } else {
+      title.innerText = "Sin información";
+      weight.innerText = "Peso: Sin información";
+      height.innerText = "Altura: Sin información";
+      lifeSpan.innerText = "Esperanza de vida: Sin información";
+    }
+  }
+  
+  button.onclick = getDogs2;
+  
+  getDogs2();
